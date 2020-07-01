c717430fc8d2d395ac8ba49aec33938f041053a7
NabeelahY <noblenab13@gmail.com>
2020-07-01 17:01:58 +0100

    Seperate components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import React, { useState } from 'react';
-import { Predictions } from 'aws-amplify';
+import React from 'react';
+import ExtractText from './components/ExtractText';
+import ImgUpload from './components/ImgUpload';
 
 function App() {
-  const [res, setRes] = useState('');
-
-  const identify = async (event) => {
-    setRes('Identifying');
-    const {
-      target: { files },
-    } = event;
-
-    const file = files[0];
-    const data = await Predictions.identify({
-      text: { source: { file }, format: 'PLAIN' },
-    });
-
-    setRes(data.text.fullText);
-  };
   return (
     <div>
-      <h3>Convert to Text</h3>
-      <input type="file" onChange={identify} />
-      <p>{res}</p>
+      <ExtractText />
+      <hr />
+      <ImgUpload />
     </div>
   );
 }
