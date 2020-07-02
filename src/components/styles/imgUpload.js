4183e9b01a19c87c72f7c05f921364a8ca93599a
NabeelahY <noblenab13@gmail.com>
2020-07-02 16:51:00 +0100

    Cleanup component and add extra styles

---
+++ src/components/styles/imgUpload.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Bottom = styled.div`
+  margin: 20px 0;
+  h3 {
+    font-size: 24px;
+    text-align: center;
+  }
+
+  input[type='file'] {
+    margin: 20px 0;
+    width: 100%;
+  }
+
+  .ant-progress {
+    margin: 0 0 20px 0;
+  }
+
+  @media (min-width: 769px) {
+    h3 {
+      font-size: 32px;
+    }
+  }
+`;
