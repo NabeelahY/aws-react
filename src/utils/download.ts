01fa35e6391709b0d73e412b1e27e403375e00e7
NabeelahY <noblenab13@gmail.com>
2021-01-03 12:13:13 +0100

    feat: add consoles and error checking

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -1,11 +1,21 @@
 import { Storage } from 'aws-amplify';
 
-const download = (file: string, setUrl: any) => {
-  Storage.get(`${file}.txt`)
-    .then((data) => {
-      setUrl(data);
-    })
-    .catch((err) => err.response);
+const download = async (file: string, setUrl: any) => {
+  try {
+    let doc = await Storage.list(`${file}.txt`);
+    console.log('doc', doc);
+    if (doc) {
+      Storage.get(`${file}.txt`)
+        .then((data) => {
+          setUrl(data);
+        })
+        .catch((err) => err.response);
+    } else {
+      console.log('else', doc);
+    }
+  } catch (error) {
+    console.log('err', error);
+  }
 };
 
 export default download;
